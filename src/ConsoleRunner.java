74ef3f586da658b05fc4eb3394da79ed4fa27fd8
karinaa-ll <1noname6769@gmail.com>
2024-03-15 10:31:16 +0600

    Third commit

---
+++ src/ConsoleRunner.java
@@ -0,0 +1,9 @@
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        int key=123
+        String text="Привет"
+        String result=".................."
+        Application application = new Application(args);
+        Result result=application.run();
+    }
+}
